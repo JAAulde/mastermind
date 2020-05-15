c615d177046c1624daeed6bda2998f5624cf9b1f
Jim Auldridge <jim.auldridge@wowza.com>
2020-05-15 00:44:18 -0400

    add param checking for Value id

--- src/lib/Value.js
+++ src/lib/Value.js
@@ -2,7 +2,9 @@ import _ from 'lodash';
 
 export default class Value {
     constructor(id, properties) {
-        this.id = id;
+        if (!_.isNumber(id) && !_.isString(id)) {
+            throw 'Value constructor requires a string or number for the first param, `id`.';
+        }
 
         if (_.isUndefined(properties)) {
             properties = {};
@@ -10,6 +12,7 @@ export default class Value {
             throw 'Any properties to be assigned to a Value must be provided in a plain object.';
         }
 
+        this.id = id;
         _.assign(this, properties);
     }
 }
