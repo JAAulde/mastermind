703b09739adf42014155c91065cb0f0ea49227c9
Jim Auldridge <jim.auldridge@wowza.com>
2020-05-14 23:44:53 -0400

    get var statements above other code

--- src/lib/Secret.js
+++ src/lib/Secret.js
@@ -7,27 +7,34 @@ export default class Secret extends Code {
     }
 
     crack(guess) {
+        var instance = this,
+            values_counts,
+            guess_counts,
+            common_values,
+            contains,
+            matches;
+
         if (!(guess instanceof Guess)) {
             throw 'A guess provided to Secret.crack must be an instance of Guess';
         }
 
-        var instance = this,
-            values_counts = _.countBy(this.values, 'id'),
-            guess_counts = _.countBy(guess.values, 'id'),
-            common_values = _.intersection(
-                _.keys(values_counts),
-                _.keys(guess_counts)
-            ),
-            contains = _.reduce(common_values, function (sum, id){
-                var addend = values_counts[id] <= guess_counts[id]
-                    ? values_counts[id]
-                    : guess_counts[id];
+        values_counts = _.countBy(this.values, 'id');
+        guess_counts = _.countBy(guess.values, 'id');
+        common_values = _.intersection(
+            _.keys(values_counts),
+            _.keys(guess_counts)
+        );
+        contains = _.reduce(common_values, function (sum, id){
+            var addend = values_counts[id] <= guess_counts[id]
+                ? values_counts[id]
+                : guess_counts[id];
 
-                return sum + addend;
-            }, 0),
-            matches = _.filter(guess.values, function(value, index){
-                return value.id === instance.values[index].id;
-            }).length;
+            return sum + addend;
+        }, 0);
+
+        matches = _.filter(guess.values, function(value, index){
+            return value.id === instance.values[index].id;
+        }).length;
 
         return {
             guess: guess,
